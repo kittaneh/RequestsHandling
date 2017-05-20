2de09ee7b6ab58aa13ead252e1faaa5fb93478c9
yahya <kittaneh@gmail.com>
2017-05-20 03:31:19 +0300

    -- code cleaning

--- grails-app/controllers/requestshandling/HomeController.groovy
+++ grails-app/controllers/requestshandling/HomeController.groovy
@@ -15,23 +15,25 @@ class HomeController {
 
     def slice(){
         requestsSlicerService.sliceAccessLogFile("data/sample_access_log_2")
+        return  [msg:"$actionName daemon initialized"]
     }
 
     def parse(){
         String slicePath = grailsApplication.config.requestsSlicer.slicePath
         requestsSlicerService.parseLogSlices(slicePath)
+        return  [msg:"$actionName daemon initialized"]
     }
 
     def simulate(){
-        def sources = ['10.10.3.4','dc1.mv.com']
+        def sources = ['10.10.3.4','dc1.mv.com','hc.webisaba.com','127.0.0.1','ping.yahoo.com','foo.bar.net']
         requestsSimulatorService.simulate(sources)
+        return  [msg:"$actionName daemon initialized"]
     }
 
     def top(){
         int id = params.id ? params.int('id'):-1
         def returnedDtata = [:]
         returnedDtata.put("top",(requestSourceService.topFreqSources(id)  as JSON))
-       // println requestSourceService.topFreqSources(id) as JSON
         return [returnedData:returnedDtata]
     }
 
--- grails-app/views/home/simulate.gsp
+++ grails-app/views/home/simulate.gsp
@@ -12,6 +12,6 @@
 </head>
 
 <body>
-
+     ${msg}
 </body>
 </html>
