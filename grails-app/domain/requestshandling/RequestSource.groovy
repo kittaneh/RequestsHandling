19e32604d3b964f337f19199ddc485d60fc803b3
yahya <kittaneh@gmail.com>
2017-05-20 05:23:52 +0300

    readme

--- grails-app/domain/requestshandling/RequestSource.groovy
+++ grails-app/domain/requestshandling/RequestSource.groovy
@@ -2,13 +2,17 @@ package requestshandling
 
 import groovy.transform.ToString
 
-
+/**
+ *  @purpose: ORM RequestSource domain class
+ *  @author: Yahya Kittaneh
+ *  @email: kittaneh@gmail.com
+ */
 @ToString
 class RequestSource {
 
-    String requestId
+    String requestId  //UUID request id
     String userAgent
-    String source
+    String source  // source domainName or IP
 
     static constraints = {
         requestId nullable: true
---
+++ test/unit/requestshandling/RequestsSimulatorServiceSpec.groovy
@@ -0,0 +1,20 @@
+package requestshandling
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(RequestsSimulatorService)
+class RequestsSimulatorServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
