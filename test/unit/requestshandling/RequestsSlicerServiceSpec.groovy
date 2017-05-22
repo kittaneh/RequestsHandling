8e91626c67d8398d1aa362b11eee1bfb13b0a561
yahya <kittaneh@gmail.com>
2017-05-22 16:40:28 +0300

    unit testing

--- test/unit/requestshandling/RequestsSlicerServiceSpec.groovy
+++ test/unit/requestshandling/RequestsSlicerServiceSpec.groovy
@@ -1,6 +1,7 @@
 package requestshandling
 
 import grails.test.mixin.TestFor
+import spock.lang.Shared
 import spock.lang.Specification
 
 /**
@@ -9,12 +10,40 @@ import spock.lang.Specification
 @TestFor(RequestsSlicerService)
 class RequestsSlicerServiceSpec extends Specification {
 
+
+    // global properties to share among all specs
+    @Shared requestSourceService
+
+    /**
+     *  In TEST environment you should manually inject
+     *  any spring beans (even grails services)
+     */
+    def doWithSpring = {
+        requestSourceService(RequestSourceService)
+    }
+
+    /**
+     * putting all shared assignations and initializations to all specs
+     */
     def setup() {
+        requestSourceService = grailsApplication.mainContext.getBean('requestSourceService')
     }
 
-    def cleanup() {
-    }
 
-    void "test something"() {
+
+    void "test slicing"() {
+
+        given:
+        String fileName = "data/localhost_access_log"
+
+        when:
+        service.sliceAccessLogFile(fileName)
+
+
+        then:
+        new File('web-app/temp/slice0').exists()
+
+
+
     }
 }
